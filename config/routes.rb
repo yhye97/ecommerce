d0f1894845e4fe3a920b8a8ba3f1d28154294a37
Hye Won Yoon <yhye97@naver.com>
2018-06-05 10:48:28 +0000

    ecommerce

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   resources :products
   resources :order_items
   resource :carts, only: [:show]
   
   root "home#index"
+  
+  get 'admins/sign_up'=>'/home/index'
+  get '/users/sign_up'=> '/home/index'
+  get '/products' => "products#new"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
